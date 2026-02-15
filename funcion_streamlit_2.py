c71305b0c46bce5ff46ecbbee935ee1bc4e9b307
FelixNavia1992 <fpnavia@gmail.com>
2026-02-15 16:45:04 -0500

    Update funcion_streamlit_2.py

--- funcion_streamlit_2.py
+++ funcion_streamlit_2.py
@@ -941,7 +941,7 @@ class PorticoAnalyzer:
 
         
         fig.add_trace(go.Scatter(x=tA, y=agA, mode="lines", showlegend=False), row=2, col=2)                            # Add trace for ground acceleration with TMD
-        fig.add_trace(go.Scatter(x=[tA[0]], y=[agA[0]], mode="markers", marker=dict(size=10), name="t (ag)),
+        fig.add_trace(go.Scatter(x=[tA[0]], y=[agA[0]], mode="markers", marker=dict(size=10), name="t (ag)"),
                     row=2, col=2)                                                                                       # Add marker for current time with TMD
 
         
@@ -951,7 +951,7 @@ class PorticoAnalyzer:
 
         
         fig.add_trace(go.Scatter(x=tA, y=respA_con, mode="lines", showlegend=False), row=3, col=2)
-        fig.add_trace(go.Scatter(x=[tA[0]], y=[respA_con[0]], mode="markers", marker=dict(size=10), name="t_with_TMD (resp)),
+        fig.add_trace(go.Scatter(x=[tA[0]], y=[respA_con[0]], mode="markers", marker=dict(size=10), name="t_with_TMD (resp)"),
                     row=3, col=2)                                                                                       # Add marker for current response with TMD
 
         frames = []                                                                                                     # Initialize frames list
@@ -1145,6 +1145,7 @@ class PorticoAnalyzer:
             st.plotly_chart(fig_acc, use_container_width=False)                                                         # Display the absolute acceleration plot
 
         
+
 
 
 
