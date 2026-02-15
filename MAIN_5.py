75a4b72d17348c7b62bbb4a54e1a4739696bfcd7
Bryan Guzmán <bryan2104guzman@gmail.com>
2026-02-15 13:47:01 -0500

    Update MAIN_5.py

--- MAIN_5.py
+++ MAIN_5.py
@@ -322,7 +322,6 @@ with tab1:
         st.image("FOTO/FOTO_FPNG.png")
         st.markdown("- [Félix Navia](https://github.com/FelixNavia1992)")
         ### **You can find me on**
-        [![Web Page](https://img.shields.io/badge/Web%20Page-bdgr.net-blue)](http:bdgr.net)
         [![GitHub Bryan Guzmán](https://img.shields.io/github/followers/Dagu2104?label=follow&style=social)](https://github.com/Dagu2104)
 
 
@@ -426,6 +425,7 @@ with tab4:                                                                      
 st.markdown("---")                                                                                                  # Horizontal separator
 
 st.markdown("#### *Developed by Eng. Félix Patricio Navia Garcia / Eng. Bryan David Guzmán Ruano — Supervisor: Eng. Carlos Andrés Celi Sánchez, M.Sc*") ## Developers credit
+
 
 
 
